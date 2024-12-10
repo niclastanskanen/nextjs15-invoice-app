0da3fa01a0a552df77e893fb958208056c187d55
Niclas <niclastanskanen@hotmail.com>
2024-12-10 10:18:38 +0100

    add DashboardPage basic styling

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,8 @@
+import Link from "next/link";
+import { CirclePlus } from "lucide-react";
+
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import {
   Table,
   TableBody,
@@ -10,24 +15,46 @@ import {
 
 const DashboardPage = () => {
   return (
-    <main className="flex flex-col justify-center h-full text-center gap-6 max-w-5xl mx-auto">
-      <h1 className="text-5xl font-bold">Dashboard</h1>
+    <main className="flex flex-col justify-center h-full text-center gap-6 max-w-5xl mx-auto my-12">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-bold">Invoices</h1>
+        <p>
+          <Button variant="ghost" asChild>
+            <Link href="/invoices/new">
+              <CirclePlus className="h-4 w-4" />
+              Create Invoice
+            </Link>
+          </Button>
+        </p>
+      </div>
       <Table>
         <TableCaption>A list of your recent invoices.</TableCaption>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[100px]">Invoice</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Method</TableHead>
-            <TableHead className="text-right">Amount</TableHead>
+            <TableHead className="w-[100px] p-4">Date</TableHead>
+            <TableHead className="p-4">Customer</TableHead>
+            <TableHead className="p-4">Email</TableHead>
+            <TableHead className="text-center p-4">Status</TableHead>
+            <TableHead className="text-right p-4">Value</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           <TableRow>
-            <TableCell className="font-medium">INV001</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell className="text-right">$250.00</TableCell>
+            <TableCell className="font-medium text-left p-4">
+              <span className="font-semibold">10/12/2024</span>
+            </TableCell>
+            <TableCell className="text-left p-4">
+              <span className="font-semibold">Niclas T</span>
+            </TableCell>
+            <TableCell className="text-left p-4">
+              <span>niclas@invoice.com</span>
+            </TableCell>
+            <TableCell className="text-center p-4">
+              <Badge className="rounded-full">Open</Badge>
+            </TableCell>
+            <TableCell className="text-right p-4">
+              <span className="font-semibold">$250.00</span>
+            </TableCell>
           </TableRow>
         </TableBody>
       </Table>
