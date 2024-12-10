9b0f4b33e2f339562d406d4c7ba8c50fa8f7364a
Niclas <niclastanskanen@hotmail.com>
2024-12-10 10:00:41 +0100

    add shadcn ui components and basic routing

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,38 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+const DashboardPage = () => {
+  return (
+    <main className="flex flex-col justify-center h-full text-center gap-6 max-w-5xl mx-auto">
+      <h1 className="text-5xl font-bold">Dashboard</h1>
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Invoice</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Method</TableHead>
+            <TableHead className="text-right">Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell className="text-right">$250.00</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </main>
+  );
+};
+
+export default DashboardPage;
