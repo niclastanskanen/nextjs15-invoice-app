234eefb7e9d1b00b83362af90b7966d0bd0eeaf0
Niclas <niclastanskanen@hotmail.com>
2024-12-13 13:01:16 +0100

    add server action for createInvoice

---
+++ actions/actions.ts
@@ -0,0 +1,24 @@
+"use server";
+
+import { redirect } from "next/navigation";
+
+import { db } from "@/db";
+import { Invoices } from "@/db/schema";
+
+export async function createInvoice(formData: FormData) {
+  const value = Math.floor(parseFloat(String(formData.get("value"))) * 100);
+  const description = formData.get("description") as string;
+
+  const results = await db
+    .insert(Invoices)
+    .values({
+      value,
+      description,
+      status: "open",
+    })
+    .returning({
+      id: Invoices.id,
+    });
+
+  redirect(`/invoices/${results[0].id}`);
+}
