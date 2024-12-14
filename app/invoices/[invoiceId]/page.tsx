538366ad49739be3707cc4aa37c041d6c89a8626
Niclas <niclastanskanen@hotmail.com>
2024-12-14 12:37:05 +0100

    add InvoiceIdPage using params to get invoiceId

---
+++ app/invoices/[invoiceId]/page.tsx
@@ -0,0 +1,72 @@
+import { eq } from "drizzle-orm";
+
+import { db } from "@/db";
+import { Invoices } from "@/db/schema";
+import { cn } from "@/lib/utils";
+
+import { Badge } from "@/components/ui/badge";
+
+const InvoiceIdPage = async ({ params }: { params: { invoiceId: string } }) => {
+  const { invoiceId: invoiceIdParam } = await params;
+  const invoiceId = parseInt(invoiceIdParam);
+
+  const [result] = await db
+    .select()
+    .from(Invoices)
+    .where(eq(Invoices.id, invoiceId))
+    .limit(1);
+
+  return (
+    <main className="h-full max-w-5xl mx-auto my-12">
+      <div className="flex justify-between mb-8">
+        <h1 className="flex items-center gap-4 text-3xl font-bold">
+          Invoice {invoiceId}{" "}
+          <Badge
+            className={cn(
+              "rounded-full capitalize",
+              result.status === "open" && "bg-blue-500",
+              result.status === "paid" && "bg-green-500",
+              result.status === "void" && "bg-zinc-700",
+              result.status === "uncollectible" && "bg-red-600",
+            )}
+          >
+            {result.status}
+          </Badge>
+        </h1>
+      </div>
+      <p className="text-3xl mb-3">${(result.value / 100).toFixed(2)}</p>
+      <p className="text-lg mb-8">{result.description}</p>
+
+      <h2 className="font-bold text-lg mb-4">Billing Details</h2>
+
+      <ul className="grid gap-2">
+        <li className="flex gap-4">
+          <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+            Invoice Id
+          </strong>
+          <span>{invoiceId}</span>
+        </li>
+        <li className="flex gap-4">
+          <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+            Invoice Date
+          </strong>
+          <span>{new Date(result.createTs).toLocaleDateString()}</span>
+        </li>
+        <li className="flex gap-4">
+          <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+            Billing Name
+          </strong>
+          <span></span>
+        </li>
+        <li className="flex gap-4">
+          <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+            Billing Email
+          </strong>
+          <span></span>
+        </li>
+      </ul>
+    </main>
+  );
+};
+
+export default InvoiceIdPage;
