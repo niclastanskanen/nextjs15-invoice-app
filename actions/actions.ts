7876aa1cbef4317d1f41ed20a2e0484ca24b74ff
Niclas <niclastanskanen@hotmail.com>
2024-12-15 15:28:03 +0100

    add userId check on createInvoice server action

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,11 +1,18 @@
 "use server";
 
 import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
 
 import { db } from "@/db";
 import { Invoices } from "@/db/schema";
 
 export async function createInvoice(formData: FormData) {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return;
+  }
+
   const value = Math.floor(parseFloat(String(formData.get("value"))) * 100);
   const description = formData.get("description") as string;
 
@@ -14,6 +21,7 @@ export async function createInvoice(formData: FormData) {
     .values({
       value,
       description,
+      userId,
       status: "open",
     })
     .returning({
