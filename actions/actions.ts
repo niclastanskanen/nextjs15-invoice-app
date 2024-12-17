4be1185a746324bd12897c1aba9c465ee7416e88
Niclas <niclastanskanen@hotmail.com>
2024-12-17 14:43:49 +0100

    add customerId to createInvoice action

--- actions/actions.ts
+++ actions/actions.ts
@@ -4,7 +4,7 @@ import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs/server";
 
 import { db } from "@/db";
-import { Invoices, Status } from "@/db/schema";
+import { Customers, Invoices, Status } from "@/db/schema";
 import { and, eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 
@@ -17,6 +17,19 @@ export async function createInvoice(formData: FormData) {
 
   const value = Math.floor(parseFloat(String(formData.get("value"))) * 100);
   const description = formData.get("description") as string;
+  const name = formData.get("name") as string;
+  const email = formData.get("email") as string;
+
+  const [customer] = await db
+    .insert(Customers)
+    .values({
+      name,
+      email,
+      userId,
+    })
+    .returning({
+      id: Customers.id,
+    });
 
   const results = await db
     .insert(Invoices)
@@ -24,6 +37,7 @@ export async function createInvoice(formData: FormData) {
       value,
       description,
       userId,
+      customerId: customer.id,
       status: "open",
     })
     .returning({
